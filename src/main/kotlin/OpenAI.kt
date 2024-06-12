2e2f957d498b91ca815e9138b823f00eb5949069
Zino Feindt-Riggers <zino.feindt-riggers@proclane.com>
2024-06-12 12:06:18 +0200

    created class ChatLog

--- src/main/kotlin/OpenAI.kt
+++ src/main/kotlin/OpenAI.kt
@@ -7,14 +7,31 @@ import kotlin.time.Duration.Companion.seconds
 
 val apiKey = "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"
 
+class ChatLog { //dient zur Aufnahme und Kontextspeicherung des User-Inputs und Bot-Response. Vorraussichtlich wird User "U:" und Bot "B:" sein.
+    var chatLog = """"""
+
+    fun writeToHistory(questionOrAnswer: String, userOrBot: Char) {
+        chatLog += "$userOrBot: $questionOrAnswer"
+    }
+}
 
 class OpenAI {
+    val LogOfInstance = ChatLog()
 
     val config = OpenAIConfig(
         token = apiKey,
         timeout = Timeout(socket = 60.seconds),
-
-        )
-
+    )
     val openAI = OpenAI(config)
+
+    fun genResponse(question: String) {
+        LogOfInstance.writeToHistory(question, 'U')
+        val responsePlacehoder = "12345"
+        LogOfInstance.writeToHistory(responsePlacehoder, 'B')
+        return
+    }
+}
+
+fun main() {
+    OpenAI()
 }
